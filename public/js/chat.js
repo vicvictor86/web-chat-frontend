15e399aa4affdb334d870d4e843dd3f8936fdd9b
Victor Emmanuel <vicbem@hotmail.com>
2022-11-04 19:28:48 -0300

    Refactoring emits

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,14 +8,43 @@ const usernameDiv = document.getElementById("username");
 socket.emit('select_room', {
   user_id: window.localStorage.getItem("user_id"),
   roomName: room,
-  connectionMessage: " entrou na sala",
 }, data => {
-  usernameDiv.innerHTML = `Olá ${data.username} - você está na sala ${room}`;
   window.localStorage.setItem("room_id", data.room_id);
+  createWelcomeMessage(data);
 
+  getOnlineUsers();
+});
+
+function getOnlineUsers(){
+  socket.emit("connections_room", {
+    room_id: window.localStorage.getItem("room_id"),
+  }, data => {
+    const onlineUsers = [];
+    data.connections.forEach(connection => {
+      if(connection.is_on_chat) {
+        onlineUsers.push(connection.user);
+      }
+    })
+  
+    createHtmlOnlineUsers(onlineUsers);
+  });
+}
+
+socket.emit('previous_messages', {
+  user_id: window.localStorage.getItem("user_id"),
+  roomName: room,
+}, data => {
   data.messages.forEach(message => {
     createMessage(message);
   });
+})
+
+socket.emit("connections_room", {
+  user_id: window.localStorage.getItem("user_id"),
+  room_id: window.localStorage.getItem("room_id"),
+}, data => {
+  const userInRoom = data[0];
+  createConnectionMessage(userInRoom);
 });
 
 document.getElementById("message_input").addEventListener("keypress", event => {
@@ -55,11 +84,46 @@ function createMessage(data) {
   `
 }
 
-document.getElementById("logout").addEventListener("click", () => {
+function createHtmlOnlineUsers(usersInCurrentRoom) {
+  const onlineUsers = document.getElementById("online-users");
+  usersInCurrentRoom.forEach(user => {
+    onlineUsers.innerHTML += `
+    <div class="new_message">
+    <label class="form-label">
+      <strong> ${user.username} </strong>
+    </label>
+    </d;
+  `;
+  });
+}
+
+function createConnectionMessage(userInRoom){
+  console.log(userInRoom);
+  if(!userInRoom.is_on_chat){
+    const dataToBack = {
+      user_id: window.localStorage.getItem("user_id"),
+      roomName: room,
+      text: " entrou na sala",
+    }
+
+    socket.emit('message', dataToBack);
+  }
+}
+
+function disconnectFromRoom(){
   const user_id = window.localStorage.getItem("user_id");
   const room_id = window.localStorage.getItem("room_id");
   const connectionMessage = " saiu da sala";
 
   socket.emit('disconnect_room', {user_id, room_id, connectionMessage });
+}
+
+function createWelcomeMessage(data){
+  usernameDiv.innerHTML = `Olá ${data.username} - você está na sala ${room}`;
+}
+
+document.getElementById("logout").addEventListener("click", () => {
+  disconnectFromRoom();
+
   window.location.href = "./select-room.html";
 });
